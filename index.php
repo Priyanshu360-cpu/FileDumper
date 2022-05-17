6a5f228888f03a3cd1289d0ba8882210c20f9425
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-05-18 00:09:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,128 @@
 <html lang="en">
+  <style>
+  .button {
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+html{
+  background-size: cover;
+  height: 100%;
+  overflow: hidden;
+  background:url(https://media.istockphoto.com/photos/blue-digital-folder-background-picture-id165858706?k=20&m=165858706&s=612x612&w=0&h=XsVYgDGVyiUXl2uI27S3OEhR7UHU0UGPeH8bZ7700wQ=)  ;
+}
+.button1 {background-color: #4CAF50;}
+.button::before {
+  content: "🔎 ";
+}
+.left{
+  background:url(https://cdn.dribbble.com/users/1514097/screenshots/3722746/location-loading.gif);
+  background-position:center;
+  width:100%;
+  height: 50px;
+  position: relative;
+  border: 0cm;
+}
+.maina{
+  text-shadow: 2px 2px #ff0000;
+  text-decoration-color: aqua;
+  font-size: 32px;
+  color:white;
+}
+.boxer{
+  text-align: center;
+}
+.shadow-textarea textarea.form-control::placeholder {
+    font-weight: 300;
+}
+.shadow-textarea textarea.form-control {
+    padding-left: 0.8rem;
+}
+.boda{
+ position: absolute;
+ left: 30%;
+ top: 38%;
+}
+.sidemenu{
+  position:absolute;
+  left:1%;
+  top:10%
+}
+    .trial{
+      position:center;
+    }
+.sidemenu:hover{
+  cursor:pointer;
+  width:25%;
+  height:1450%;
+  background-position:left;
+ 
+  background:url(https://i.pinimg.com/originals/05/c7/9e/05c79ee812c45a7535f749cf5e49e94e.png) repeat;
+}
+
+.imaga{
+  position:absolute;
+  left:86%;
+  top:15%
+}
+.imaga:hover{
+  cursor: pointer;
+}
+.imagai{
+  position:absolute;
+  left:90%;
+  top:10%
+}
+.imagai:hover{
+  cursor: pointer;
+}
+.imagag{
+  position:absolute;
+  left:93%;
+  top:-57%
+}
+.imagag:hover{
+  cursor: pointer;
+
+}
+    @media screen and (max-width: 600px) {
+   .sidemenu {
+      width: 100%;
+   }
+
+</style>
+  
 <head>
 <Title>File Upload Tutorial</Title>
 </head>
+  <div class="tempbg">
+<div class="left"><div class="sidemenu"><image src="https://cdn.discordapp.com/emojis/970780591169630278.png?size=32"></image></div><div class="imaga"><a href="https://play.google.com/store"><image src="https://cdn.discordapp.com/emojis/976190753103548446.png?size=32"></image></a></div><div class="imagai"><a href="https://www.instagram.com/priyanshu.__0007/"><image src="https://cdn.discordapp.com/emojis/797052330054516747.gif?size=32"></image></a></div><div class="imagag"><a href="mailto:tpriyanshu775@gmail.com"><image src="https://cdn.discordapp.com/emojis/775178764715950100.gif?size=64"></image></a></div></div>
+<div class="boda">
+
 <body>
- 
 	<form action="server.php" method="post" enctype="multipart/form-data">
+    <div class="maina"><h2>File Dumper</h2></div>
+    <textarea class="form-control z-depth-1" id="w3review"  name="w3review" rows="2" cols="80" placeholder="Enter Roll"></textarea>
 	<p>
-	<label for="my_upload">Select the file to upload:</label>
+    
+	<label for="my_upload" style=" text-shadow: 2px 2px #ff0000;
+  text-decoration-color: aqua;
+  font-size: 22px;
+  color:white;">Upload File Here.:</label>
 	<input id="my_upload" name="my_upload" type="file">
-    <textarea id="w3review" name="w3review" >
-    </textarea>
+   
+      
+
 	</p>
 	<input type="submit" value="Upload Now">
 	</form>
 </body>
+  </div>
+  </div>
 </html>
