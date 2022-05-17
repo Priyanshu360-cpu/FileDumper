bb17d194b7e6f6958cac69431e5cf83507120165
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-05-17 22:33:33 +0530

    Create server.php

---
+++ server.php
@@ -0,0 +1,28 @@
+<?php 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') 
+{
+ 
+  echo explode('.',$_FILES['my_upload']['name'])[1] ;
+  if (is_uploaded_file($_FILES['my_upload']['tmp_name'])) 
+  { 
+  	if(empty($_FILES['my_upload']['name']))
+  	{
+  		echo " File name is empty! ";
+  		exit;
+  	}
+
+  	$upload_file_name = $_FILES['my_upload']['name'];
+  	if(strlen ($upload_file_name)>100)
+  	{
+  		echo " too long file name ";
+  		exit;
+  	}
+
+  	$upload_file_name = preg_replace("/[^A-Za-z0-9 \.\-_]/", '', $upload_file_name);
+$dest=__DIR__.'/uploads/'.htmlentities($_POST['w3review']).'.'.explode('.',$_FILES['my_upload']['name'])[1] ;;
+    if (move_uploaded_file($_FILES['my_upload']['tmp_name'], $dest)) 
+    {
+    	echo 'File Has Been Uploaded !';
+    }
+  }
+}
